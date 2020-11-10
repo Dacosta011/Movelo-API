c6b5303b1001b1d02218b6531226d8478d4eef42
David Acosta <davidacostag01@gmail.com>
2020-11-10 12:26:17 -0500

    post

--- src/main/java/com/init/moveloapi/restTest.java
+++ src/main/java/com/init/moveloapi/restTest.java
@@ -1,6 +1,11 @@
 package com.init.moveloapi;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +16,14 @@ public class restTest {
 	
 	@GetMapping
 	public String hello() {
-		return "hello world";
+		return "holaaaaa";
+	}
+	
+	@PostMapping
+	public ResponseEntity<user> login(@RequestBody user usuario){
+		user us = new user();
+		us.setCorreo(usuario.getCorreo());
+		us.setClave(usuario.getClave());
+		return ResponseEntity.ok(us);
 	}
 }
---
+++ src/main/java/com/init/moveloapi/user.java
@@ -0,0 +1,22 @@
+package com.init.moveloapi;
+
+public class user {
+	
+	private String correo;
+	private String clave;
+	
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getClave() {
+		return clave;
+	}
+	public void setClave(String clave) {
+		this.clave = clave;
+	}
+	
+	
+}
