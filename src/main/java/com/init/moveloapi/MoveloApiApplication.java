db707c07aee678bea3d34b9aa6900b8c375c21a4
David Acosta <davidacostag01@gmail.com>
2020-11-10 11:33:59 -0500

    first commit

---
+++ src/main/java/com/init/moveloapi/MoveloApiApplication.java
@@ -0,0 +1,13 @@
+package com.init.moveloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoveloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoveloApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/init/moveloapi/MoveloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.init.moveloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoveloApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
